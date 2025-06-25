c2be3dde6a1637b561a06857b0c0e612d0599858
Apoorva Shrivastava <apoorvashrivs@gmail.com>
2025-06-25 14:24:18 +0530

    Update client.py

--- Communication With Peer/Peers/client.py
+++ Communication With Peer/Peers/client.py
@@ -1,40 +1,46 @@
-import TxBlock
+# client.py
 import Transactions
 import Signatures
-import pickle 
+import pickle
 import socket
+import time
+import random
 
 TCP_PORT = 5005
 
-def sendBlock(ip_addr, blk):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip_addr, TCP_PORT))
-    data = pickle.dumps(blk)
-    s.sendall(data)
-    s.close()
-    return False
+def sendObj(ip_addr, obj):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip_addr, TCP_PORT))
+        data = pickle.dumps(obj)
+        s.sendall(data)
+        s.close()
+    except Exception as e:
+        print("Send failed:", e)
 
 if __name__ == "__main__":
-    pr1, pu1 = Signatures.generate_keys()
-    pr2, pu2 = Signatures.generate_keys()
-    pr3, pu3 = Signatures.generate_keys()
+    print("Client started... Generating random transactions")
+    
+    # Generate 5 wallet key pairs
+    wallets = [Signatures.generate_keys() for _ in range(5)]
 
-    Tx1 = Transactions.Tx()
-    Tx1.add_input(pu1, 2.3)
-    Tx1.add_output(pu2, 1.0)
-    Tx1.add_output(pu3, 1.1)
-    Tx1.sign(pr1)
+    while True:
+        sender_idx = random.randint(0, 4)
+        receiver_idx = random.randint(0, 4)
+        while receiver_idx == sender_idx:
+            receiver_idx = random.randint(0, 4)
 
-    Tx2 = Transactions.Tx()
-    Tx2.add_input(pu3, 2.3)
-    Tx2.add_input(pu2, 1.0)
-    Tx2.add_output(pu1, 3.1)
-    Tx2.sign(pr2)
-    Tx2.sign(pr3)
+        pr_sender, pu_sender = wallets[sender_idx]
+        _, pu_receiver = wallets[receiver_idx]
 
-    B1 = TxBlock.TxBlock(None)
-    B1.addTx(Tx1)
-    B1.addTx(Tx2)
+        amount = round(random.uniform(1.0, 5.0), 2)
 
-    sendBlock('localhost', B1)
-    sendBlock('localhost', Tx2)
+        tx = Transactions.Tx()
+        tx.add_input(pu_sender, amount)
+        tx.add_output(pu_receiver, amount - 0.1)  # simulate fee
+        tx.sign(pr_sender)
+
+        print(f"Sending {amount} from Wallet {sender_idx} to {receiver_idx}")
+        sendObj('localhost', tx)
+
+        time.sleep(2)  # Wait before next tx
